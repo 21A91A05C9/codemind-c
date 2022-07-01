27c2c744ebf30fc8265c1faa60d0482a11b5e036
21A91A05C9 <97424395+21A91A05C9@users.noreply.github.com>
2022-07-01 11:47:50 +0530

    #include<stdio.h> #include<math.h> int main() {     int i,n,arr[100],e=0,o=0,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             e=e+arr[i];         }         else         {             o=o+arr[i];         }     }     sum=abs(o-e);     printf("%d",sum); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,n,arr[100],e=0,o=0,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            e=e+arr[i];
+        }
+        else
+        {
+            o=o+arr[i];
+        }
+    }
+    sum=abs(o-e);
+    printf("%d",sum);
+}
